f832f516e24fd2ffcf6048c543926ab6d9ec32fd
nejda <nejda@DESKTOP-F1MIGHB>
2017-09-29 00:30:36 +0300

    хз

---
+++ ConsoleApplication3/ConsoleApplication3/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+   public class Jenkia
+    {
+        public int b;
+        public string name, family, adress;
+        public void writetoconsole(string name, string family, string adress, int b)
+        {
+            Console.WriteLine(" имя: {0}\n фамилия: {1}\n адресс: {2} \n\n", name, family, adress);
+        }
+
+
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Jenkia my = new Jenkia();
+
+                my.name = "jenia";
+                my.family = "lol";
+                my.adress = "keke";
+                my.b = 19;
+
+            Jenkia me = new Jenkia();
+
+                 me.name = "Evgesha";
+                 me.family = "keker";
+                 me.adress = "luler";
+                 me.b = 2;
+
+            my.writetoconsole(me.name, me.family, me.adress, me.b);
+            my.writetoconsole(my.name, my.family, my.adress, my.b);
+            Console.ReadKey();
+
+
+        }
+    }
+}
